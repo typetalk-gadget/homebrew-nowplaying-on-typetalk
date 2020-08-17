308603370003d496df4b331fa32111d1c944f806
Yuichi Watanabe <yuichi.watanabe.ja@gmail.com>
2020-08-17 09:20:47 +0900

    Update nowplaying_on_typetalk.rb

--- nowplaying_on_typetalk.rb
+++ nowplaying_on_typetalk.rb
@@ -1,8 +1,8 @@
 class NowplayingOnTypetalk < Formula
     desc "A command line tool to display the song currently playing on Spotify in Typetalk status"
-    url "https://github.com/typetalk-gadget/nowplaying-on-typetalk/releases/download/v0.3.0/nowplaying-on-typetalk_v0.3.0_darwin_amd64.zip"
-    sha256 "6ed8e561d7bb8f6ed9bbe6dd4b42e9825074d83879ed8cdfb7b342c28bbd6b76"
-    version "v0.3.0"
+    url "https://github.com/typetalk-gadget/nowplaying-on-typetalk/releases/download/v0.3.1/nowplaying-on-typetalk_v0.3.1_darwin_amd64.zip"
+    sha256 "b84968b2cbc380867670f04bc74f91eeff97393a14f786931abc51208a7003e1"
+    version "v0.3.1"
     def install
         bin.install 'nowplaying-on-typetalk'
     end
